243fd622df251fedd059116cd53821d46391a44e
Thanh <thanhcap3@gmail.com>
2025-05-12 23:56:04 +0700

    fix: pagination lesson logic

--- vocab-app/hooks/useLessons.ts
+++ vocab-app/hooks/useLessons.ts
@@ -2,13 +2,14 @@ import { useState, useEffect, use } from "react";
 import { Course, Lesson } from "@/types/lesson-types";
 import { getCoursesByPage, getLessonsByCourseId } from "@/services/course-service";
 
+const NUM_LESSONS_PER_PAGE = 6;
 export function useLessons(themeId: string | null) {
   const [lessons, setLessons] = useState<Lesson[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [nextPage, setNextPage] = useState<number | null>(null);
   const [prevPage, setPrevPage] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
-
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     if (!themeId) {
@@ -19,10 +20,12 @@ export function useLessons(themeId: string | null) {
     async function fetchLessons() {
       setIsLoading(true);
       try {
-        const response = await getLessonsByCourseId(themeId, currentPage); 
+        const response = await getLessonsByCourseId(themeId, currentPage, NUM_LESSONS_PER_PAGE); 
+        console.log("Lấy danh sách bài học thành công:", response);
         setLessons(response.results);
         setNextPage(response.next ? currentPage + 1 : null); // cập nhật trang tiếp theo
         setPrevPage(response.previous ? currentPage - 1 : null); // cập nhật trang trước đó
+        setTotalPages(Math.ceil(response.count / NUM_LESSONS_PER_PAGE)); 
       } catch (error) {
         console.error("Lỗi khi lấy danh sách bài học:", error);
       }
@@ -30,7 +33,7 @@ export function useLessons(themeId: string | null) {
     }
 
     fetchLessons();
-  }, [themeId, currentPage]); 
+  }, [themeId, currentPage]);
 
-  return { lessons, isLoading, nextPage, prevPage, currentPage, setCurrentPage, setPrevPage };
+  return { lessons, isLoading, nextPage, prevPage, currentPage, setCurrentPage, setPrevPage, totalPages };
 }
