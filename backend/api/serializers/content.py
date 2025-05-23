56b4414294f1cc6f8edd81ce4d2030fa14985fc8
dotuangv <dovantuan.gv2004@gmail.com>
2025-05-24 00:30:42 +0700

    Update avatar Leaderboard

--- backend/api/serializers/content.py
+++ backend/api/serializers/content.py
@@ -56,7 +56,11 @@ class LeaderBoardSerializer(serializers.ModelSerializer):
     Serializer cho bảng xếp hạng người dùng.
     """
     username = serializers.CharField(source='user.username', read_only=True)
+    avatar = serializers.SerializerMethodField()
     class Meta:
         model = LeaderBoard
-        fields = ['username', 'total_score']
-        read_only_fields = ['username', 'total_score']
+        fields = ['username', 'total_score', 'avatar']
+        read_only_fields = ['username', 'total_score', 'avatar']
+
+    def get_avatar(self, obj):
+        return obj.user.avatar
