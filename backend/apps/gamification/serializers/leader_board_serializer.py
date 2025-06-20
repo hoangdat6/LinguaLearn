922c9bb1cf5909be5d522cd6642467b27697901a
hoangdat123 <dathv2004@gmail.com>
2025-06-20 11:13:05 +0700

    update docker compose

--- backend/apps/gamification/serializers/leader_board_serializer.py
+++ backend/apps/gamification/serializers/leader_board_serializer.py
@@ -1,5 +1,9 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
+from rest_framework.fields import CharField
+
 from ..models import LeaderBoard
+
 
 class LeaderBoardSerializer(serializers.ModelSerializer):
     """
@@ -12,5 +16,7 @@ class LeaderBoardSerializer(serializers.ModelSerializer):
         fields = ['username', 'total_score', 'avatar']
         read_only_fields = ['username', 'total_score', 'avatar']
 
+
+    @extend_schema_field(CharField)
     def get_avatar(self, obj):
         return obj.user.avatar
